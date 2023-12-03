da94c13fefee85aab45c228c741d929feb00603b
Tuyishimes <53575313+Tuyishimes@users.noreply.github.com>
2023-12-03 16:15:28 +0200

    Configure React app for deployment to GitHub Pages

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <div className='bg-white content-center justify-center text-black'>
+            <nav>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default NavBar
